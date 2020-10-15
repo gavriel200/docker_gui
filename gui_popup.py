2ebe0e84ac1adcc620f26c41212f6a52d202803f
gabi <gavriel.gav@gmail.com>
2020-10-15 16:43:31 +0300

    finished commenting

--- gui_popup.py
+++ gui_popup.py
@@ -23,69 +23,83 @@ class Make_popup():
         error_massage = Make_label(self.popup, self.text, 18, "red", "#0e1733")
         error_massage.pack()
 
-# ---------------- images page --------------------------------------------------- #
+# ---------------- images page popup --------------------------------------------- #
+    # ---------------- run ------------------------- #
     def run_image_popup(self, rep_tag, reload_page):
         self.rep_tag = rep_tag
+        # -------- reload main gui ------ #
         self.reload_page = reload_page
-        self.port = []
-        self.vol = []
-        self.name = []
+        
         self.bash = False
         self.popup.geometry("350x350")
 
+        self.port = []
+        self.vol = []
+        self.name = []
+        
         self.main_frame = Frame(self.popup, width=350, height=300, bg="#0e1733")
         self.main_frame.pack()
-
+        
+        # -------- main page build ------ #
         def update_page():
-            # -------------------------------- start by removing the old frame ---------------------------------------------------------------- #
+        # ---------------- frame remove old add new ---- #
+            # -------- removing old frame ----#
             try:
                 self.main_frame.destroy()
             except:
                 pass
-            # -------------------------------- adding the new main frame to work on ----------------------------------------------------------- #
+            # -------- add new frame -------- #
             self.main_frame = Frame(self.popup, width=350, height=350, bg="#0e1733")
             self.main_frame.pack()
 
-            # ------------------------- adding the other frame where the vols and ports are displayed ----------------------------------------- #
+            # -------- add frame for color -- #
             self.frame_for_the_color = Frame(self.main_frame,  width=330, height=80, bg="#344658")
             self.frame_for_the_color.place(x = 10, y = 160)
+
+            # -------- add frame vols ports - #
             self.port_vol_frame = Frame(self.main_frame,  width=330, height=80, bg="#344658")
             self.port_vol_frame.place(x = 10, y = 160)
 
-            # -------------------------------- label to show what docker image are we running -------------------------------------------------- #
+            # -------- docker image name ---- #
             self.docker_run_label = Make_label(self.main_frame, "docker run " + self.rep_tag, 11, "white", "#0e1733")
             self.docker_run_label.place(15, 20)
 
-            # -------------- making the labels host and container to show the user whitch entry is whitch --------------------------------------- #
+            # -------- host and conainer ---- #
             self.host_label = Make_label(self.main_frame, "host", 15, "white", "#0e1733")
             self.host_label.place(30,93)
             self.cont_label = Make_label(self.main_frame, "container", 13, "white", "#0e1733")
             self.cont_label.place(126,93)
 
-            # ----------------------------------------------------------- adding the entry ------------------------------------------------------------------------ #
-            # ------------------------------------------ entry and add button for the ports ---------------------------------------------------- #
+        # ---------------- ports ----------------------- #
+            # -------- entry ports ---------- #
             self.port_host = Entry(self.main_frame, width = 10)
             self.port_host.place(x = 10, y = 60)
             self.port_cont = Entry(self.main_frame, width = 10)
             self.port_cont.place(x = 130, y = 60)
             self.port_host_cont = Make_label(self.main_frame, ":", 18, "White", "#0e1733")
             self.port_host_cont.place(105, 60)
+
+            # -------- add port but --------- #
             self.port_add = Button(self.main_frame, text="add port", bg="#344658", font=("Courier",9), fg = "white", padx=21, 
             command = lambda : port_add_function(self.port_host.get(), self.port_cont.get()), cursor="hand2")
             self.port_add.place(x = 235, y = 60)
 
-            # ------------------------------------------ entry and add button for the vols ----------------------------------------------------- #
+        # ---------------- volumes --------------------- #
+            # -------- entry vols ----------- #
             self.vol_host = Entry(self.main_frame, width = 10)
             self.vol_host.place(x = 10, y = 120)
             self.vol_cont = Entry(self.main_frame, width = 10)
             self.vol_cont.place(x = 130, y = 120)
             self.vol_host_cont = Make_label(self.main_frame, ":", 18, "White", "#0e1733")
             self.vol_host_cont.place(105, 120)
+
+            # -------- add volumes but ------ #
             self.vol_add = Button(self.main_frame, text="add vol", bg="#344658", font=("Courier",9), fg = "white", padx=25, 
             command = lambda : vol_add_function(self.vol_host.get(), self.vol_cont.get()), cursor="hand2")
             self.vol_add.place(x = 235, y = 120)
             
-            # -------------------------------- list the ports and the volumes that were added -------------------------------------------------- #
+        # ---------------- added ports and vols -------- #
+            # -------- vols and ports label - #
             self.ports_list = Make_label(self.port_vol_frame, "ports: ", 12, "white", "#344658")
             self.ports_list.grid(0,0)
             self.space = Make_label(self.port_vol_frame, "", 12, "white", "#344658")
@@ -93,6 +107,7 @@ class Make_popup():
             self.vols_list = Make_label(self.port_vol_frame, "vols: ", 12, "white", "#344658")
             self.vols_list.grid(0,2)
 
+            # -------- ports and vols list -- #
             col = 1
             for items in self.port:
                 self.port_from_list = Remove_vols_and_ports(self.port_vol_frame, items, self.port, update_page)
@@ -104,20 +119,26 @@ class Make_popup():
                 self.vol_from_list.grid(col, 2)
                 col=col+1
 
-            # --------------------------------------- display name entry and label ------------------------------------------------------------- #
+        # ---------------- name ------------------------ #
+            # -------- entry name ----------- #
             self.name_label = Make_label(self.main_frame, "name:", 16, "white", "#0e1733")
             self.name_label.place(10, 257)
             self.name_entry = Entry(self.main_frame, width=20)
             self.name_entry.place(x = 80, y = 255)
+
+            # -------- add chosen name ------ #
             try:
                 self.name_entry.insert(0,self.name[0].split(" ")[1])
             except:
                 pass
+
+            # -------- add name but --------- #
             self.name_add_button = Button(self.main_frame, text="ADD", bg="#344658", font=("Courier",14), fg = "white", cursor="hand2", 
             command=lambda:name_add_function(self.name_entry.get()))
             self.name_add_button.place(x = 250, y = 250)
 
-            # ----------------------------- display the bash button in one color if bash is active if not the in another ----------------------- #
+        # ---------------- bash ------------------------ #
+            # -------- change bash color ---- #
             if self.bash == False:
                 self.bash_button = Button(self.main_frame, text="bash", bg="#344658", font=("Courier",18), fg = "white", command = bash_button_function, cursor="hand2")
                 self.bash_button.place(x = 10, y = 300)
@@ -127,21 +148,25 @@ class Make_popup():
                 self.running_in_bash = Make_label(self.main_frame, "running bash", 16, "white", "#0e1733")
                 self.running_in_bash.place(100, 310)
             
-            # ---------------------------------------- the run button  ------------------------------------------------------------------------- #
+        # ---------------- run ------------------------- #
             self.run_button = Button(self.main_frame, text="RUN", bg="#344658", font=("Courier",18), fg = "white", command=run_button_function, cursor="hand2")
             self.run_button.place(x = 270, y = 300)
 
+        # -------- add ports and vols --- #
         def port_add_function(host_entry, cont_entry):
             try:
+                # -------- check if int -------- #
                 int(host_entry)
                 int(cont_entry)
                 new_port = f"-p {host_entry}:{cont_entry}"
+                # -------- check if exists ------ #
                 if new_port in self.port:
                     self.port_host.delete(0, END)
                     self.port_cont.delete(0, END)
                     error_popup = Make_popup("ERROR")
                     error_popup.error("ERROR! make sure that you write both the\nhost and the container port\nand that you dont have two of the same ports")
                     self.popup.grab_set()
+                # -------- check if spaces ------ #
                 elif " " in host_entry or " " in cont_entry:
                     self.port_host.delete(0, END)
                     self.port_cont.delete(0, END)
@@ -163,6 +188,7 @@ class Make_popup():
         def vol_add_function(host_entry, cont_entry):
             try:
                 new_vol = f"-v {host_entry}:{cont_entry}"
+                # -------- check if exists ------ #
                 if new_vol in self.vol:
                     self.vol_host.delete(0, END)
                     self.vol_cont.delete(0, END)
@@ -181,18 +207,23 @@ class Make_popup():
                 error_popup.error("ERROR!")
                 self.popup.grab_set()                
 
+        # -------- bash on off ---------- #
         def bash_button_function():
+            # -------- true/false bash ------ #
             if self.bash == False:
                 self.bash = True
             else:
                 self.bash = False
             update_page()
         
+        # -------- add image name ------- #
         def name_add_function(name_entry):
+            # -------- check if spaces ------ #
             if " " in name_entry:
                 error_popup = Make_popup("ERROR")
                 error_popup.error("ERROR! you cant have spaces in the container name use _")
                 self.popup.grab_set()
+            # -------- check if too long ---- #
             elif len(name_entry) > 13:
                 error_popup = Make_popup("ERROR")
                 error_popup.error("ERROR! you should not make names so long")
@@ -207,7 +238,9 @@ class Make_popup():
                     self.name.append(f"--name {name_entry}")
                     update_page()
 
+        # -------- run command ---------- #
         def run_button_function():
+            # -------- check name ----------- #
             if len(self.name)==0:
                 error_popup = Make_popup("ERROR")
                 error_popup.error("ERROR! you should name your container so you could find it later")
@@ -219,14 +252,17 @@ class Make_popup():
                 self.reload_page()
                 self.popup.destroy()
 
+        # -------- start the page ------- #
         update_page()
 
+    # ---------------- edit ------------------------ #
     def edit_image_popup(self, rep_tag, reload_page):
         self.rep_tag = rep_tag
         self.reload_page = reload_page
 
         self.popup.geometry("340x250")
 
+        # -------- old image name ------- #
         self.old_label = Make_label(self.popup, "OLD", 18, "white", "#0e1733")
         self.old_label.place(20, 20)
 
@@ -240,6 +276,7 @@ class Make_popup():
             self.rep_tag_label = Make_label(self.old_frame, self.rep_tag, 15, "white", "#344658")
             self.rep_tag_label.place(70, 10)
 
+        # -------- new image name ------- #
         self.new_label = Make_label(self.popup, "NEW", 18, "white", "#0e1733")
         self.new_label.place(20, 105)
 
@@ -252,33 +289,41 @@ class Make_popup():
         self.seperate_new = Make_label(self.popup, ":", 19, "white", "#0e1733")
         self.seperate_new.place(160, 135)
 
+        # -------- edit button ---------- #
         self.edit_button = Button(self.popup, text="EDIT", bg="#344658", font=("Courier",26), fg = "white", width=13, height=1, 
         command=lambda:edit_button_function(self.new_rep.get(),self.new_tag.get()), cursor="hand2")
         self.edit_button.place(x = 20, y = 180)
 
+        # -------- edit command ---------- #
         def edit_button_function(new_rep, new_tag):
+            # -------- check if rep name ---- #
             if len(new_rep) == 0:
                 error_popup = Make_popup("ERROR")
                 error_popup.error("ERROR! make sure you write a reposotry name")
                 self.popup.grab_set()
+            # -------- check rep len -------- #
             if len(new_rep) > 13:
                 error_popup = Make_popup("ERROR")
                 error_popup.error("ERROR! you should not make names so long")
                 self.popup.grab_set()
+            # -------- check rep spaces ----- #
             elif " " in new_rep:
                 error_popup = Make_popup("ERROR")
                 error_popup.error("ERROR! should not have spaces in rep name")
                 self.popup.grab_set()
             else:
+                # -------- check tag spaces ----- #
                 if " " in new_tag:
                     error_popup = Make_popup("ERROR")
                     error_popup.error("ERROR! should not have spaces in tag name")
                     self.popup.grab_set()
+                # -------- check tag len -------- #
                 elif len(new_tag) >13:
                     error_popup = Make_popup("ERROR")
                     error_popup.error("ERROR! you should not make tags so long")
                     self.popup.grab_set()
                 elif len(new_tag) == 0:
+                    # -------- edit rep:latest ------ #
                     new_rep_tag = new_rep
                     edit = Docker_images().change_image_tag(self.rep_tag, new_rep_tag)
                     if edit[0] == 1:
@@ -290,6 +335,7 @@ class Make_popup():
                         self.reload_page()
                         self.popup.destroy()
                 else:
+                    # -------- edit rep:tag --------- #
                     new_rep_tag = new_rep+":"+new_tag
                     edit = Docker_images().change_image_tag(self.rep_tag, new_rep_tag)
                     if edit[0] == 1:
@@ -301,6 +347,7 @@ class Make_popup():
                         self.reload_page()
                         self.popup.destroy()
 
+    # ---------------- save ------------------------ #
     def save_image_popup(self, rep_tag, reload_page):
         self.rep_tag = rep_tag
         self.reload_page = reload_page
@@ -310,6 +357,7 @@ class Make_popup():
         self.rep_tag_label = Make_label(self.popup, self.rep_tag, 14, "white", "#0e1733")
         self.rep_tag_label.place(20, 17)
 
+        # -------- choose dir ----------- #
         self.choose_dir_button = Button(self.popup, text="choose dir", bg="#344658", font=("Courier",15), 
         fg = "white", width=25, height=0, cursor="hand2",command=lambda:choose_dir_button_function())
         self.choose_dir_button.place(x = 20, y = 50)
@@ -320,16 +368,19 @@ class Make_popup():
         self.slesh_label = Make_label(self.popup, "/", 18, "white", "#0e1733")
         self.slesh_label.place(150 ,102)
 
+        # -------- file name ------------ #
         self.file_name = Entry(self.popup, width = 15)
         self.file_name.place(x = 170, y = 100)
 
         self.tar_label = Make_label(self.popup, ".tar", 14, "white", "#0e1733")
         self.tar_label.place(300 ,103)
 
+        # -------- save button ----------- #
         self.save_button = Button(self.popup, text="SAVE", bg="#344658", font=("Courier",24), 
         fg = "white", width=5, height=1, cursor="hand2", command=lambda:save_button_function())
         self.save_button.place(x = 220,y = 155)
 
+        # -------- choose dir function -- #
         def choose_dir_button_function():
             self.dir = filedialog.askdirectory(initialdir = "~", title = "Select a Folder")
             self.chosen_dir.config(state=NORMAL)
@@ -337,23 +388,28 @@ class Make_popup():
             self.chosen_dir.insert(0,self.dir)
             self.chosen_dir.config(state=DISABLED)
 
+        # -------- save command --------- #
         def save_button_function():
+            # -------- check dir exists ----- #
             if len(self.chosen_dir.get()) == 0:
                 error_popup = Make_popup("ERROR")
                 error_popup.error("ERROR! your file should have a directory")
                 self.popup.grab_set()
             else:
+                # -------- check name exists ---- #
                 self.file = self.file_name.get()
                 if len(self.file) == 0:
                     self.file_name.delete(0, END)
                     error_popup = Make_popup("ERROR")
                     error_popup.error("ERROR! your file should have a name...")
                     self.popup.grab_set()
+                # -------- check no / ----------- #
                 elif "/" in self.file: # verry importand in windows should add more symbols / \ : ? * " < > |
                     self.file_name.delete(0, END)
                     error_popup = Make_popup("ERROR")
                     error_popup.error("ERROR! you cant have / in your file name!!")
                     self.popup.grab_set()
+                # -------- check spaces --------- #
                 elif " " in self.file:
                     self.file_name.delete(0, END)
                     error_popup = Make_popup("ERROR")
@@ -371,21 +427,26 @@ class Make_popup():
                         self.reload_page()
                         self.popup.destroy()
 
+    # ---------------- remove ---------------------- #
     def rm_image_popup(self, rep_tag, reload_page):
         self.rep_tag = rep_tag
         self.reload_page = reload_page
 
         self.popup.geometry("300x100")
 
+        # -------- rep name ------------- #
         self.label = Make_label(self.popup, f"are you sure you want to remove\n{rep_tag}?", 10, "white", "#0e1733")
         self.label.place(25,10)
 
+        # -------- cancel button -------- #
         self.cancel_button = Button(self.popup, text="cancel", bg="#344658", font=("Courier",15), fg = "white", width=5, height=0, command=self.popup.destroy,  cursor="hand2")
         self.cancel_button.place(x = 10,y = 55)
 
+        # -------- remove button -------- #
         self.rm_button =  Button(self.popup, text="REMOVE", bg="#344658", font=("Courier",15), fg = "white", width=5, height=0, command=lambda:rm_button_function(),  cursor="hand2")
         self.rm_button.place(x = 200,y = 55)
 
+        # -------- remove command ------- #
         def rm_button_function():
             rm = Docker_images().rm_image(self.rep_tag)
             if rm[0] == 1:
@@ -397,7 +458,9 @@ class Make_popup():
                 self.reload_page()
                 self.popup.destroy()
 
-# ---------------- container page ------------------------------------------------ #
+# ---------------- containers page popup ----------------------------------------- #
+
+# ---------------- sea page popup ------------------------------------------------ #
 
 class Remove_vols_and_ports():
 # ---------------- create vols and ports objects --------------------------------- #
